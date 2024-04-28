5fb915961214420bbc2f505154632b4844855d5f
Eric Chen <kych@berkeley.edu>
2024-04-28 05:14:27 +0000

    Update Dockerfile to use keplerc/cloud_robotics_tutorial:base as base image, add build steps for FogROS2 and cloudgripper-ros, and update docker-run.sh

--- Dockerfile
+++ Dockerfile
@@ -5,18 +5,14 @@ ARG USER
 ENV HOME=${HOME}
 ENV USER=${USER}
 # ENV WORKDIR="$(cd $(dirname "$0") ; pwd)"
+ENV CYCLONEDDS_URI=file:///fog_ws/src/FogROS2/fogros2/configs/cyclonedds.ubuntu.2204.xml
 
 # Create FogROS2 worspace and build it
-ENV ROS_WS=${HOME}/workshop_ws
+ENV ROS_WS=/fog_ws
 RUN mkdir -p ${ROS_WS}/src
 WORKDIR ${ROS_WS}/src
 RUN git clone https://github.com/BerkeleyAutomation/FogROS2.git
 RUN git clone https://github.com/cloudgripper/cloudgripper-ros.git
-
-
-WORKDIR ${ROS_WS}
-RUN . /opt/ros/${ROS_DISTRO}/setup.sh && \
-      colcon build --cmake-clean-cache
 
 ENTRYPOINT [ "/ros_entrypoint.sh" ]
 CMD ["bash"]
--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-WORKDIR="$(cd $(dirname "$0") ; pwd)"
+CURR_DIR="$(cd $(dirname "$0") ; pwd)"
+ROS_WORKDIR="/fog_ws"
 
 docker run -it --rm \
        -e USER_ID=`id -u $USER` \
        -e GROUP_ID=`id -g $USER` \
-       -e WORKDIR="$WORKDIR" \
+       -e WORKDIR="$ROS_WORKDIR" \
        --net=host \
        -v "$HOME/.aws:$HOME/.aws" \
        --cap-add=NET_ADMIN \
-       --mount src="$WORKDIR",target="$WORKDIR",type=bind \
-       --workdir "$PWD" \
+       --mount src="$CURR_DIR",target="$ROS_WORKDIR/src/CloudRobotics_tutorial",type=bind \
+       --workdir "$ROS_WORKDIR" \
        cloudrobotics_tutorial "$@"
